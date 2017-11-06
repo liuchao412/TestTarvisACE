dc7a504019ea425c0c36186a550a5ab8b6d3dbf4
freeeyes <freeeyes@163.com>
2017-11-06 10:51:21 +0800

    添加测试脚本

---
+++ mian.cpp
@@ -0,0 +1,16 @@
+#include "ace/OS_NS_sys_time.h"
+#include "ace/CDR_Stream.h"
+#include "ace/INET_Addr.h"
+#include "ace/SOCK_Connector.h"
+#include "ace/SOCK_Stream.h"
+#include "ace/Log_Msg.h"
+#include "ace/Log_Record.h"
+#include "ace/Truncate.h"
+#include "ace/OS_NS_unistd.h"
+#include "ace/OS_NS_stdlib.h"
+
+int ACE_TMAIN(int argc, ACE_TCHAR* argv[])
+{
+	ACE_DEBUG((LM_INFO, "Complite OK.\n"));
+	return 0;
+}
